fcffe47884885a6f228104f88adfdc9cee938968
AhmadAlmudhi <a.almudhi1419@gmail.com>
2023-05-23 20:56:56 +0300

    Fix: edit some response messages

--- homework_11/bin/responses/auth_responses/login_response.dart
+++ homework_11/bin/responses/auth_responses/login_response.dart
@@ -1,26 +1,30 @@
 import 'dart:convert';
-
 import 'package:shelf/shelf.dart';
-
 import '../../response_messages/bad_request.dart';
 import '../../response_messages/success.dart';
 import '../../services/supabase/supabase_env.dart';
 
-loginHandler(Request req) async{
+loginHandler(Request req) async {
   try {
     final body = json.decode(await req.readAsString());
     if (body["email"] == null || body["password"] == null) {
-      return Response.badRequest(body: "make sure to enter the email and password");
+      return Response.badRequest(
+        body: "make sure to enter the email and password",
+      );
     }
     final auth = SupabaseEnv().supabase.auth;
     final userLogin = await auth.signInWithPassword(
       email: body["email"],
       password: body["password"],
-    ); 
+    );
 
     return Success().responseMessage(
-        message: "success",
-        data: {"token": userLogin.session?.accessToken.toString(),"refresh_Token": userLogin.session?.refreshToken.toString()},);
+      message: "You are now logged in.",
+      data: {
+        "token": userLogin.session?.accessToken.toString(),
+        "refresh_Token": userLogin.session?.refreshToken.toString(),
+      },
+    );
   } catch (error) {
     return BadRequest().responseMessage(message: 'something went wrong');
   }
--- homework_11/bin/responses/user_responses/display_user_response.dart
+++ homework_11/bin/responses/user_responses/display_user_response.dart
@@ -1,30 +1,29 @@
 import 'package:shelf/shelf.dart';
-
 import '../../response_messages/bad_request.dart';
 import '../../response_messages/success.dart';
 import '../../services/supabase/supabase_env.dart';
 
-displayUserHandler(Request _, String id) async{
-  try{
-  final supabase = SupabaseEnv().supabase;
-  
-  final user = (await supabase
-      .from("profiles")
-      .select()
-      .eq("id", int.parse(id)))[0];
-  final contact =
-      await supabase
-      .from("contact")
-      .select()
-      .eq("id_user", int.parse(id));
-  
-  Map userMap = {...user, "contact": contact,};
+displayUserHandler(Request _, String id) async {
+  try {
+    final supabase = SupabaseEnv().supabase;
 
-  return Success().responseMessage(message: "account",data: userMap);
-  }on RangeError{
+    final user =
+        (await supabase.from("profiles").select().eq("id", int.parse(id)))[0];
+    final contact =
+        await supabase.from("contact").select().eq("id_user", int.parse(id));
+
+    Map userMap = {
+      ...user,
+      "contact": contact,
+    };
+
+    return Success().responseMessage(
+      message: "Here are all of the user information.",
+      data: userMap,
+    );
+  } on RangeError {
     return BadRequest().responseMessage(message: "user does not exist");
-  }
-  catch(error){
+  } catch (error) {
     return BadRequest().responseMessage(message: "somthing went wrong");
   }
 }
